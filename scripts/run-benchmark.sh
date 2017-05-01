fb38c5c67bbacd8071fbdd0f352ffdfea9f2c76b
ops <ops@mr-0xc8.0xdata.loc>
2017-05-01 12:35:04 -0700

    try parameterized benchmark

--- scripts/run-benchmark.sh
+++ scripts/run-benchmark.sh
@@ -1,7 +1,11 @@
+NGPU=$1
+
 cd /opt/h2oaiglm/src
 make -j gpu cpu
 cd /opt/h2oaiglm/examples/cpp
 ln -sf /data/train.txt .
-export N=16
+
+echo "Starting Benchmark on $NGPU GPUs"
+export N=$NGPU
 make run 2>&1 | tee log$(N).txt
 cp log$(N).txt /data
