6fbfea29b51840fb98b74565c6bcce67293f1a13
ops <ops@mr-0xc8.0xdata.loc>
2017-05-01 15:49:14 -0700

    change cosmetics and file paths

--- scripts/run-benchmark.sh
+++ scripts/run-benchmark.sh
@@ -1,14 +1,21 @@
 export CUDA_HOME=/usr/local/cuda-8.0
 export PATH=$CUDA_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+export N=$1
+
+DATADIR=/data/benchmarks_gpu$N_`date +%Y%m%d_%H%M``
 
 cd /opt/h2oaiglm/src
 make -j gpu cpu
 cd /opt/h2oaiglm/examples/cpp
 ln -sf /data/train.txt .
 
-echo "Starting Benchmark on $NGPU GPUs"
-export N=$1
 make run 2>&1 | tee log$N.txt
-tar -cvf benchmarks_gpu$1_`date +%Y%m%d_%H%M%S`.tar `find . -name "me*.txt"`
-mv *.tar /data
+
+mkdir $DATADIR
+cp me*.txt $DATADIR
+cp log*.txt $DATADIR
+
+rm me*.txt
+rm log*.txt
+
