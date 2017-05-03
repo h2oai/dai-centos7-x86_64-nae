317659b1c7283453241ce302fe349a318f7fc01f
ops <ops@mr-0xc8.0xdata.loc>
2017-05-03 12:54:53 -0700

    wrap in sudo

--- scripts/start-h2oai.sh
+++ scripts/start-h2oai.sh
@@ -2,8 +2,10 @@
 
 cd /opt/h2oai && /usr/bin/python3.6 -m h2o
 
-sed -e 's/8888/12345/' -i /etc/nginx/site-enabled/default
-sed -e 's/8888/12345/' -i /etc/nginx/sites-enabled/notebook-site
+sudo sed -e 's/8888/12345/' -i /etc/nginx/site-enabled/default
+sudo sed -e 's/8888/12345/' -i /etc/nginx/sites-enabled/notebook-site
+
+sudo /etc/init.d/nginx restart
 
 # Start Notebook
 /usr/local/bin/nimbix_notebook
