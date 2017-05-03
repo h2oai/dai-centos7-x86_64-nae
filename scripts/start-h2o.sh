2fd089f7e025eb40f2be1195576295a309e681f3
ops <ops@mr-0xc8.0xdata.loc>
2017-05-03 12:32:05 -0700

    added nginx flop

--- scripts/start-h2o.sh
+++ scripts/start-h2o.sh
@@ -29,3 +29,6 @@ then
 fi
 
 java -Xmx${xmxMb}m -jar /opt/h2o3-xgboost/h2o.jar -name H2ODemo -flatfile flatfile.txt -port 54321 ${hdfs_config_option} ${hdfs_config_value} ${hdfs_option} ${hdfs_option_value} ${hdfs_version}
+
+sed -e 's/8888/54321/' -i /etc/nginx/sites-enabled/default
+sed -e 's/8888/54321/' -i /etc/nginx/sites-enabled/notebook-site
--- scripts/start-h2oai.sh
+++ scripts/start-h2oai.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 cd /opt/h2oai && /usr/bin/python3.6 -m h2o
+
+sed -e 's/8888/12345/' -i /etc/nginx/site-enabled/default
+sed -e 's/8888/12345/' -i /etc/nginx/sites-enabled/notebook-site
+
+# Start Notebook
+/usr/local/bin/nimbix_notebook
