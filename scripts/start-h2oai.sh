b432bf845897fbdb8f553b9fad9f5f230d18c326
ops <ops@mr-0xc8.0xdata.loc>
2017-05-05 11:26:15 -0700

    change h2oai to not start notebook

--- scripts/start-h2oai.sh
+++ scripts/start-h2oai.sh
@@ -3,9 +3,7 @@
 # Change Nginx Redirect
 sudo sed -e 's/8888/12345/' -i /etc/nginx/sites-enabled/default
 sudo sed -e 's/8888/12345/' -i /etc/nginx/sites-enabled/notebook-site
+sudo /usr/sbin/nginx
 
-# Start Notebook
-/usr/local/bin/nimbix_notebook
-
-cd /opt/h2oai 
+cd /opt/h2oai  
 /usr/bin/python3.6 -m h2o 
