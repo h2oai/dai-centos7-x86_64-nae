b2198c16035b14ecadc612a3fdd934b34317e6e2
ops <ops@mr-0xc8.0xdata.loc>
2017-05-09 09:32:51 -0700

    ensure removal of /etc/NAE/url.txt

--- scripts/start-h2o.sh
+++ scripts/start-h2o.sh
@@ -8,8 +8,8 @@ sudo sed -e 's/8888/54321/' -i /etc/nginx/sites-enabled/notebook-site
 sudo service ssh restart
 sudo /usr/sbin/nginx
 
-rm -f /etc/NAE/url.txt
-echo "http://%PUBLICADDR%:54321/" > /etc/NAE/url.txt
+sudo rm -f /etc/NAE/url.txt
+sudo echo "http://%PUBLICADDR%:54321/" > /etc/NAE/url.txt
 
 # Use 90% of RAM for H2O.
 memTotalKb=`cat /proc/meminfo | grep MemTotal | sed 's/MemTotal:[ \t]*//' | sed 's/ kB//'`
--- scripts/start-h2oai.sh
+++ scripts/start-h2oai.sh
@@ -6,8 +6,8 @@ sudo sed -e 's/8888/12345/' -i /etc/nginx/sites-enabled/notebook-site
 sudo service ssh restart
 sudo /usr/sbin/nginx
 
-rm -f /etc/NAE/url.txt
-echo "http://%PUBLICADDR%:12345/" > /etc/NAE/url.txt
+sudo rm -f /etc/NAE/url.txt
+sudo echo "http://%PUBLICADDR%:12345/" > /etc/NAE/url.txt
 
 cd /opt/h2oai  
 /usr/bin/python3.6 -m h2o 
