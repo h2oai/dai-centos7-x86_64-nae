67816c0a7d9e47a1df991ee544be97d4db7cfd60
Nick <nkpng2k@gmail.com>
2018-03-28 15:23:44 -0700

    changed name for env variable, changes to run-dai-nimbix.sh to accept commandline input

--- run-dai-nimbix.sh
+++ run-dai-nimbix.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+while [ $# -gt 0 ]
+do
+  echo "$1"
+  shift
+done
+
 sudo nvidia-smi -pm 1
 /opt/h2oai/dai/run-h2oai.sh
-tail -f /opt/h2oai/dai/log/h2oai.out 
+tail -f /opt/h2oai/dai/log/h2oai.out
