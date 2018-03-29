1830c38053653df0ba4877b0484c5f5880d25790
Nick <nkpng2k@gmail.com>
2018-03-29 11:40:05 -0700

    startup script edits

--- run-dai-nimbix.sh
+++ run-dai-nimbix.sh
@@ -6,6 +6,16 @@ set -x
 echo "Arguement One: $1"
 echo "Arguement Two: $2"
 echo "Arguement Three: $3"
+echo "Arguement Four: $4"
+echo "Arguement Five: $5"
+
+export DRIVERLESS_AI_CONFIG_FILE_PATH=$4
+echo "$DRIVERLESS_AI_CONFIG_FILE_PATH"
+
+cp "$5" $DRIVERLESS_AI_CONFIG_FILE_PATH
+cd $DRIVERLESS_AI_CONFIG_FILE_PATH
+ls
+cd
 
 echo "Starting Driverless AI"
 sudo nvidia-smi -pm 1
