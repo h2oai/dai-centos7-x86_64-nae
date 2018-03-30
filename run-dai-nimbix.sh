4b5d0bed0a4bbc62e20f26841fbccb378dc3962d
Nick <nkpng2k@gmail.com>
2018-03-30 11:21:54 -0700

    gpu detection

--- run-dai-nimbix.sh
+++ run-dai-nimbix.sh
@@ -6,16 +6,22 @@ set -x
 export DRIVERLESS_AI_CONFIG_FILE_PATH="/opt/h2oai/dai"
 echo "$DRIVERLESS_AI_CONFIG_FILE_PATH"
 
-if [ -z "$4" ]
+if [ -z "$5" ]
 then
   echo "No Configuration File Provided"
 else
   echo "Making Configuration File Available for DAI"
-  CONFIG_FILE="$4"
+  CONFIG_FILE="$5"
   cp $CONFIG_FILE "$DRIVERLESS_AI_CONFIG_FILE_PATH/config.toml"
 fi
 
 echo "Starting Driverless AI"
-sudo nvidia-smi -pm 1
+if [ $4 -gt 0 ]
+then
+  sudo nvidia-smi -pm 1
+else
+  echo "No GPUs Available, CPU Only"
+fi
+
 /opt/h2oai/dai/run-h2oai.sh
 tail -f /opt/h2oai/dai/log/h2oai.out
