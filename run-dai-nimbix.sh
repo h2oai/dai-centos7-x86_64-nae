5eb1431469f165d7a2d61faa864661a14f9c94f6
Jeff Gambera <gambera@gmail.com>
2018-06-21 13:05:25 -0700

    add auto start DAI

--- run-dai-nimbix.sh
+++ run-dai-nimbix.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-set -e
-set -x
-
 export DRIVERLESS_AI_AUTHENTICATION_METHOD="local"
 export DRIVERLESS_AI_LOCAL_HTPASSWD_FILE="/etc/JARVICE/htpasswd"
 echo "Starting Driverless AI"
 
-tail -f /opt/h2oai/dai/log/dai.out
+sudo nvidia-smi -pm 1
+sudo systemctl dai restart
+sudo tail -f /opt/h2oai/dai/log/dai.out
