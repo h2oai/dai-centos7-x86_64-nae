979b1030573ef67e6ea965690a7f3ac1b9e9b08a
Jeff Gambera <gambera@gmail.com>
2018-06-21 13:43:01 -0700

    remove explicit start and rely on systemctl

--- run-dai-nimbix.sh
+++ run-dai-nimbix.sh
@@ -5,5 +5,5 @@ export DRIVERLESS_AI_LOCAL_HTPASSWD_FILE="/etc/JARVICE/htpasswd"
 echo "Starting Driverless AI"
 
 sudo nvidia-smi -pm 1
-sudo systemctl dai restart
+sudo systemctl restart dai
 sudo tail -f /opt/h2oai/dai/log/dai.out
